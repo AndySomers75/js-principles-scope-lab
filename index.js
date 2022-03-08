3ed18b2215e9a3271d3d17e032773e5dac4c83d0
Andrew <andrew@pureaquasprings.com>
2022-03-08 14:54:49 +1100

    commit tree 7120ecbbfe2f3c1ca86470cfd0daa89812019403

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-var customerName ='bob';{
-   console.log('bob'.toUpperCase);
+    
+var customerName = 'bob'
+const leastFavoriteCustomer = 'some initial value'
+
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase
+}
+function setBestCustomer();{
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    bestCustomer = 'billy'
 }
 
